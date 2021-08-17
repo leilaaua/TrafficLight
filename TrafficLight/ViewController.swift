ae90eea9be4d4c9b2ab13094a8837dac041aebb6
leilaaua <leila0777@icloud.com>
2021-08-17 18:38:46 +0400

    change view controller

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private let alphaValue: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = alphaValue
+        yellowLightView.alpha = alphaValue
+        greenLightView.alpha = alphaValue
+        
+        redLightView.backgroundColor = .red
+        yellowLightView.backgroundColor = .yellow
+        greenLightView.backgroundColor = .green
+        startButton.layer.cornerRadius = 10
+        
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        redLightView.layer.cornerRadius =  redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+    }
+    
+    enum LightColor {
+        case red , yellow, green
+    }
+    
+    private var currentLight = LightColor.red
+    
+    @IBAction func startButtonPressed() {
+        
+        switch currentLight {
+        case .red:
+            currentLight = LightColor.yellow
+            redLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        case .yellow:
+            currentLight = LightColor.green
+            redLightView.alpha = alphaValue
+            yellowLightView.alpha = 1
+        case .green:
+            yellowLightView.alpha = alphaValue
+            greenLightView.alpha = 1
+        }
+    }
 }
-
